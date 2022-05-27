eaea9a03a70d247e1c297df39697c1af0efc0a28
Daniel Jimenez <daniel@djimenezdev.com>
2022-05-27 02:40:35 -0700

    fixed cors url syntax error

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ app.use(
   cors({
     origin: [
       `http://localhost:3000`,
-      "https://
-djbuildspace-ethcontract.vercel.app",
+      "https://djbuildspace-ethcontract.vercel.app/",
     ],
   })
 );
