da664423ed309fa25af3fe39df30cef8626a565b
Daniel Jimenez <daniel@djimenezdev.com>
2022-05-26 23:18:03 -0700

    update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.get(
   "/",
   cors({
-    origin: [, `http://localhost:3000`],
+    origin: [`http://localhost:3000`],
   }),
   (req, res) => {
     res.send("This is the DJ BuildSpace Ethereum Contract API");
@@ -25,7 +25,7 @@ app.get(
 app.post(
   "/emailSend",
   cors({
-    origin: [, `http://localhost:3000`],
+    origin: [`http://localhost:3000`],
   }),
   async (req, res) => {
     const { email, firstName, lastName, subject, content } = req.body;
